7ab8efcb7d8557f9f2f78f0f83f8f8d8ef8df2d2
Michael Wong <mpwong@ucsb.edu>
2024-02-29 14:45:16 -0800

    mw - removed unused functions from the @testing-library/react import

--- frontend/src/tests/pages/RiderApplication/RiderApplicationShowPage.test.js
+++ frontend/src/tests/pages/RiderApplication/RiderApplicationShowPage.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, waitFor, findByText, queryByText } from "@testing-library/react";
+import {render } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import RiderApplicationShowPageMember from "main/pages/RiderApplication/RiderApplicationShowPageMember";
