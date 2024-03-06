46803ae1e86fc89091c9abed2a93a03f732f1b1e
nguyenliliana <liliananguyen@ucsb.edu>
2024-03-05 18:09:25 -0800

    ln - add story file for Admin page

---
+++ frontend/src/stories/pages/RiderApplication/RiderApplicationIndexPageAdmin.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import RiderApplicationIndexPageAdmin from 'main/pages/RiderApplication/RiderApplicationIndexPageAdmin';
+
+export default {
+   title: 'pages/RiderApplication/RiderApplicationIndexPageAdmin',
+   component: RiderApplicationIndexPageAdmin
+};
+
+const Template = () => <RiderApplicationIndexPageAdmin />;
+
+export const Default = Template.bind({});
+
+
+
+
